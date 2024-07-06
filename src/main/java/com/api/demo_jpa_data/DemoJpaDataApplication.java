a554c13def27b1b9cf0d2406047077c58eb1a820
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-06 19:08:52 -0300

    feat: add projeto Spring Boot

---
+++ src/main/java/com/api/demo_jpa_data/DemoJpaDataApplication.java
@@ -0,0 +1,13 @@
+package com.api.demo_jpa_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpaDataApplication.class, args);
+	}
+
+}

c1008c6c60ba0bc9b0f5562d438511dc8fc425e0
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-08 20:34:41 -0300

    feat: add relacionamento ManyToMany

--- src/main/java/com/api/demo_jpa_data/models/BookModel.java
+++ src/main/java/com/api/demo_jpa_data/models/BookModel.java
@@ -3,6 +3,8 @@ package com.api.demo_jpa_data.models;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -23,6 +25,16 @@ public class BookModel implements Serializable {
     @ManyToOne//(fetch = FetchType.LAZY)
     @JoinColumn(name = "publisher_id")
     private PublisherModel publisher;
+
+    // Vários livros têm vários autores
+    //@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)  // remover junto com FetchType.LAZY
+    @ManyToMany//(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "tb_book_author",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
+    private Set<AuthorModel> authors = new HashSet<>();
 
     public UUID getId() {
         return id;
@@ -46,5 +58,13 @@ public class BookModel implements Serializable {
 
     public void setPublisher(PublisherModel publisher) {
         this.publisher = publisher;
+    }
+
+    public Set<AuthorModel> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<AuthorModel> authors) {
+        this.authors = authors;
     }
 }

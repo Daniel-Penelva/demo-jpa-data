6b9bb44ea3dcab930c021f8d716a3389ea4168c4
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-08 22:55:18 -0300

    feat: add repositorio Book, Author, Publisher e Review

---
+++ src/main/java/com/api/demo_jpa_data/repositories/PublisherRepository.java
@@ -0,0 +1,11 @@
+package com.api.demo_jpa_data.repositories;
+
+import com.api.demo_jpa_data.models.PublisherModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<PublisherModel, UUID> {
+}
---
+++ src/main/java/com/api/demo_jpa_data/repositories/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.api.demo_jpa_data.repositories;
+
+import com.api.demo_jpa_data.models.ReviewModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<ReviewModel, UUID> {
+}
